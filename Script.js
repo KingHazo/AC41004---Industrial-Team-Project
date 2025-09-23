7e1ab09559ff12b21f5448e6652aafc51f6be99b
Mayorstick <111825379+Mayorstick@users.noreply.github.com>
2025-09-23 20:57:53 +0100

    Add files via upload
    Add login , index UI files (HTML, CSS, JS)

---
+++ Script.js
@@ -0,0 +1,53 @@
+const login = document.getElementById("login");
+console.log(login);
+
+login.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = "login.html"
+});
+
+
+
+//list of images used for the background 
+const images = [
+    "https://cdn.pixabay.com/photo/2024/09/21/02/13/global-business-9062781_1280.jpg",
+    "https://cdn.pixabay.com/photo/2017/05/31/11/17/office-2360063_1280.jpg",
+
+];
+
+let current = 0;
+const bg1 = document.querySelector(".bg1");
+const bg2 = document.querySelector(".bg2");
+let showingBg1 = true;
+
+
+function changeBackground() {
+    const nextImage = images[current];
+
+    if (showingBg1) {
+        bg2.style.backgroundImage = `url(${nextImage})`;
+        bg2.style.opacity = 1;
+        bg1.style.opacity = 0;
+    } else {
+        bg1.style.backgroundImage = `url(${nextImage})`;
+        bg1.style.opacity = 1;
+        bg2.style.opacity = 0;
+    }
+
+    showingBg1 = !showingBg1;
+    current = (current + 1) % images.length;
+}
+
+
+bg1.style.backgroundImage = `url(${images[0]})`;
+bg1.style.opacity = 1;
+current = 1;
+
+setInterval(changeBackground, 5000);
+
+
+let copyrightDate = document.querySelector('.day')
+
+copyrightDate.textContent = new Date().getFullYear()
+
+
