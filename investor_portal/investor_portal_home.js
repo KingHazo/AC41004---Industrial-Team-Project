febb2587c853e420e34888d83bcd8425dbd2c77d
Marylou <2501402@dundee.ac.uk>
2025-10-02 14:19:21 +0100

    removed duplicate investor portal folder

--- investor_portal/investor_portal_home.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.more-btn').forEach((btn) => {
-    btn.addEventListener('click', () => {
-        window.location.href = 'investor_pitch_details.html'; // Change path as needed
-    });
-});
--- sql/db.php
+++ sql/db.php
@@ -1,18 +1,14 @@
 <?php
-
-$host = "database.crwckgs6iy7z.us-east-1.rds.amazonaws.com";
+$host = "database-3.ctcgqoecevan.us-east-1.rds.amazonaws.com";
 $username = "admin";
-$password = "admin1234";
-$database = "AC41004_DB";
-
-$mysql = null; 
-
+$password = "admin123";
+$database = "database-3"; 
 try {
-    $mysql = new PDO("mysql:host=".$host.";dbname=".$database, $username, $password);
-
-    $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    $mysql = new PDO("mysql:host=".$host.";dbname=".$database,
+    $username, $password);
+    // echo "Database connection successful! <br>";
 } catch (PDOException $e) {
-    error_log("FATAL DB CONNECTION FAILURE: " . $e->getMessage());
+    // echo "Database connection failed: " . $e->getMessage();
+    exit(); 
 }
-
+?>
