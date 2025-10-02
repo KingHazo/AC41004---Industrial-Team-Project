0dd1d14773f4b60cfb5e16ebdebf8a5da240cba8
Marylou <2501402@dundee.ac.uk>
2025-10-02 23:45:33 +0100

    changed database details back

--- sql/db.php
+++ sql/db.php
@@ -1,14 +1,17 @@
 <?php
-$host = "database-3.ctcgqoecevan.us-east-1.rds.amazonaws.com";
+
+$host = "database.crwckgs6iy7z.us-east-1.rds.amazonaws.com";
 $username = "admin";
-$password = "admin123";
-$database = "database-3"; 
+$password = "admin1234";
+$database = "AC41004_DB";
+
+$mysql = null; 
+
 try {
-    $mysql = new PDO("mysql:host=".$host.";dbname=".$database,
-    $username, $password);
-    // echo "Database connection successful! <br>";
+    $mysql = new PDO("mysql:host=".$host.";dbname=".$database, $username, $password);
+
+    $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 } catch (PDOException $e) {
-    // echo "Database connection failed: " . $e->getMessage();
-    exit(); 
+    error_log("FATAL DB CONNECTION FAILURE: " . $e->getMessage());
 }
-?>
